c6e838d0d3c58763eb0ac272e5a52598347da535
ErunaM <DeceptiveSmurf@gmail.com>
2019-08-26 12:03:10 +1200

    Fixing mistake on uploading node modules

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,7 +5,7 @@ export default class Navigation extends Component{
   render(){
     return(
   <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
